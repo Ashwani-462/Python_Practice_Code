0c704175b311bd327d4eb22f03e94bd979efc3ce
Ashwani-462 <ashwanim1198@gmail.com>
2026-02-01 11:40:43 +0530

    Add functionality to store and display movie names

---
+++ List_of_movies.py
@@ -0,0 +1,9 @@
+#Store movies name in List
+a=input("Enter movie name: ")
+b=input("Enter movie name: ")
+c=input("Enter movie name: ")
+Movies=[]
+Movies.append(a)
+Movies.append(b)
+Movies.append(c)
+print(Movies)
